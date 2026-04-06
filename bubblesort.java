d2d658f3b7329b43d911a7dacde88556451b7188
JATIN KUSHWAHA <jatinkushwaha@gmail.com>
2026-04-06 22:52:57 +0530

    bubble sort

---
+++ bubblesort.java
@@ -0,0 +1,21 @@
+public class bubblesort {
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length- i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,4,5,5,3,2,4};
+        bubbleSort(arr);
+        for (int i =1;i<arr.length;i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
