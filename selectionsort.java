cdf462413a351af1cef6804e896159c70cc19fa7
JATIN KUSHWAHA <jatinkushwaha@gmail.com>
2026-04-07 23:39:13 +0530

    update

---
+++ selectionsort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class selectionsort {
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 2, 4, 1};
+        for (int i=0;i<arr.length-1;i++) {
+            int min=i;
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[j]<arr[min]) {
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for (int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
