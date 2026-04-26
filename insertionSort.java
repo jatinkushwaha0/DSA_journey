1427f0a288dbbbf148ef1bdfb7fbf0388399c295
JATIN KUSHWAHA <jatinkushwaha225@gmail.com>
2026-04-26 21:02:43 +0530

    insertionsort

---
+++ insertionSort.java
@@ -0,0 +1,26 @@
+class insertionsort {
+    public static void insertion(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+
+    }
+
+    public static void printing(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 2, 5, 3 };
+        insertion(arr);
+        printing(arr);
+    }
+}
