bbad6c44c536e03c03063b718ef6c42627da753b
JATIN KUSHWAHA <jatinkushwaha225@gmail.com>
2026-05-02 00:07:25 +0530

    Largest String

---
+++ LargestString.java
@@ -0,0 +1,12 @@
+public class LargestString {
+    public static void main(String[] args) {
+        String str[] = { "Apple", "Abcd", "banana" };
+        String largest = str[0];
+        for (int i = 0; i < str.length; i++) {
+            if (largest.compareTo(str[i]) < 0) {
+                largest = str[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
