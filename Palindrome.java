bba7488df6e651c49ef83d5b3313decc0d6a70f0
JATIN KUSHWAHA <jatinkushwaha225@gmail.com>
2026-05-03 00:16:52 +0530

    substring

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome{
     public static boolean isPalindrome(String str){
         int n=str.length();
-        for(int i=0;i<str.length()/2;i++){
+        for(int i=0;i<n/2;i++){
 if(str.charAt(i)!=str.charAt(n-1-i)){
 return false;
 }
---
+++ SubString.java
@@ -0,0 +1,26 @@
+public class SubString {
+
+    // method 1
+    // public static String substrings(String str, int si, int ei) {
+    // String substr = "";
+    // for (int i = si; i < ei; i++) {
+    // substr += str.charAt(i);
+    // }
+    // return substr;
+    // }
+
+    // public static void main(String[] args) {
+    // String str = "HelloWorld";
+    // System.out.println(substrings(str, 0, 5));
+    // }
+    // }
+
+
+    
+    // method 2
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println(str.substring(0, 5));
+
+    }
+}
